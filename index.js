36502b887274ca4f84a80d092958c5df9b4fb19e
wellington gadelha <contato.informeai@gmail.com>
2022-10-18 17:06:51 -0300

    chore: update code files

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ async function run(){
         const ghToken = core.getInput('gh-token');
         const octokit = github.getOctokit(ghToken)
         if(eventName == 'pull_request'){
+            console.log('Single mode active')
             if(!defaultBranch.length) {
                 core.setFailed('Default Branch invalid')
             }
@@ -18,11 +19,13 @@ async function run(){
             if(!ghToken.length) {
                 core.setFailed('Token is required')
             }
+            console.log('Get Head Commit')
             const defaultBranchCommit = await octokit.request(`GET /repos/{owner}/{repo}/commits/${defaultBranch}`, {
                 owner: github.context.repo.owner,
                 repo: github.context.repo.repo
             })
             const headCommit = defaultBranchCommit.data.sha
+            console.log('Get Commits from PR')
             const prCommits = await octokit.request(`GET /repos/{owner}/{repo}/commits?sha=${pullRequestBranch}&per_page=100`,{
                 owner: github.context.repo.owner,
                 repo: github.context.repo.repo
@@ -31,9 +34,10 @@ async function run(){
             const allCommits = prCommits.data.map((c)=> c.sha)
             const pr = github.context.payload.pull_request.number
             if(github.context.payload.pull_request.number) core.setOutput('pr-number',github.context.payload.pull_request.number)
+            console.log('Create labels if not exist')
             await command.exec('gh',['label','create','is-rebased','--description="branch actual is rebased with default branch"','--color=0E8A16','-f'])
             await command.exec('gh',['label','create','not-rebased','--description="branch actual is not rebased with default branch"','--color=B60205','-f'])
-
+            console.log('Execute verify in PR and set label')
             if (allCommits.includes(headCommit)){
                 await command.exec('gh',['pr','edit',`${pr}`,'--add-label="is-rebased"','--remove-label="not-rebased"'])
                 core.setOutput('rebased',true)
@@ -47,14 +51,17 @@ async function run(){
             const reactive = core.getBooleanInput('reactive');
             const ref = github.context.ref
             if(reactive && ref.includes(defaultBranch)){
+                console.log('Reactive mode active')
                 if(!defaultBranch.length) {
                     core.setFailed('Default Branch invalid')
                 }
+                console.log('Get Head Commit')
                 const defaultBranchCommit = await octokit.request(`GET /repos/{owner}/{repo}/commits/${defaultBranch}`, {
                     owner: github.context.repo.owner,
                     repo: github.context.repo.repo
                 })
                 const headCommit = defaultBranchCommit.data.sha
+                console.log('Get Prs from repo')
                 const resultPRS = await octokit.request(`GET /repos/{owner}/{repo}/pulls`,{
                     owner: github.context.repo.owner,
                     repo: github.context.repo.repo
@@ -65,10 +72,10 @@ async function run(){
                         number: p.number
                     }
                 })
-
+                console.log('Create labels if not exist')
                 await command.exec('gh',['label','create','is-rebased','--description="branch actual is rebased with default branch"','--color=0E8A16','-f'])
                 await command.exec('gh',['label','create','not-rebased','--description="branch actual is not rebased with default branch"','--color=B60205','-f'])
-
+                console.log('Execute verify if rebased in all prs')
                 await Promise.allSettled(allPrsBranches.map(async(branch)=>{
                     const prCommits = await octokit.request(`GET /repos/{owner}/{repo}/commits?sha=${branch.ref}&per_page=100`,{
                         owner: github.context.repo.owner,
