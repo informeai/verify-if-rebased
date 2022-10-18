254f072c93b7929a4690802e0b29e917bb576c21
wellington gadelha <contato.informeai@gmail.com>
2022-10-18 15:13:45 -0300

    chore: update code files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
+const command = require('@actions/exec');
 async function run(){
     try {
         const eventName = github.context.eventName;
@@ -37,6 +37,11 @@ async function run(){
             console.log('allCommits: ', allCommits)
             const pr = github.context.payload.pull_request.number
             console.log('pr number: ',pr)
+            await command.exec('gh',['label','create','is-rebased','--description="branch actual is rebased with default branch"','--color=0E8A16','-f'])
+            await command.exec('gh',['label','create','not-rebased','--description="branch actual is not rebased with default branch"','--color=B60205','-f'])
+            // gh label create is-rebased --description "branch actual is rebased with default branch" --color 0E8A16 -f
+            // gh label create not-rebased --description "branch actual is not rebased with default branch" --color B60205 -f
+
             if (allCommits.includes(headCommit)){
                 console.log(true)
             }else{
