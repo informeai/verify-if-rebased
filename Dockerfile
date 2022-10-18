61b3dc7abec5b319af411fcc538d9377027f40fc
wellington gadelha <contato.informeai@gmail.com>
2022-10-18 10:56:59 -0300

    chore: remove files not used

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM alpine:3.10
-
-COPY entrypoint.sh /entrypoint.sh
-COPY . .
-RUN chmod +x entrypoint.sh
-ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -l
-echo "Set PR number"
-echo "pr-number=${{github.event.pull_request.number}}" >> GITHUB_OUTPUT
-echo "Rebased"
-echo "rebased=true" >> GITHUB_OUTPUT
