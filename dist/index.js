b7063d57b5d798898d5ded2c8b54c18ea0302c23
wellington gadelha <contato.informeai@gmail.com>
2022-10-18 14:39:31 -0300

    chore: update code files

--- dist/index.js
+++ dist/index.js
@@ -9714,7 +9714,7 @@ async function run(){
               })
             console.log('headCommit: ',headCommit.data.sha)
 
-            const prCommits = await octokit.request(`GET /repos/{owner}/{repo}/commits/${pullRequestBranch}`,{
+            const prCommits = await octokit.request(`GET /repos/{owner}/{repo}/commits?sha=${pullRequestBranch}`,{
                 owner: github.context.repo.owner,
                 repo: github.context.repo.repo
             })
